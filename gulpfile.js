088a46a36299b9edfcdef79719f836973e26406a
jonathan-vallet <jonathan.vallet@adfab.fr>
2016-11-16 15:06:59 +0100

    BOILERPLATE-14: Ajout du watch des ajouts/suppressions pour le less

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,18 @@ var config      = require('./gulp-config');
 var runSequence = require('run-sequence');
 var gulp        = require('gulp');
 var del         = require('del');
-var browserSync  = require('browser-sync');
+var browserSync = require('browser-sync');
+var watch       = require('gulp-watch');
 
-//gulp.task('fonticon', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/fonticon'));
-//gulp.task('fonts', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/fonts'));
-//gulp.task('images', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/images'));
-//gulp.task('less', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/less'));
-//gulp.task('lesshint', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/lesshint'));
-//gulp.task('lib', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/lib'));
-//gulp.task('scripts', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/scripts'));
-//gulp.task('sass', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/sass'));
-//gulp.task('views', require('./node_modules/adfab-gulp-boilerplate/gulp-tasks/views'));
+//gulp.task('fonticon', require('./gulp-tasks/fonticon'));
+//gulp.task('fonts', require('./gulp-tasks/fonts'));
+//gulp.task('images', require('./gulp-tasks/images'));
+//gulp.task('less', require('./gulp-tasks/less'));
+//gulp.task('lesshint', require('./gulp-tasks/lesshint'));
+//gulp.task('lib', require('./gulp-tasks/lib'));
+//gulp.task('scripts', require('./gulp-tasks/scripts'));
+//gulp.task('sass', require('./gulp-tasks/sass'));
+//gulp.task('views', require('./gulp-tasks/views'));
 
 /**
  * Clean build directory
@@ -21,7 +22,7 @@ gulp.task('clean', function(cb) {
     del([
 //        config.destination.assetsFolder + config.destination.cssFolderName,
 //        config.destination.assetsFolder + config.destination.fontFolderName,
-//        config.destination.assetsFolder + config.destination.imagesFolderName
+//        config.destination.assetsFolder + config.destination.imagesFolderName,
 //        config.destination.assetsFolder + config.destination.jsFolderName,
 //        config.destination.assetsFolder + config.destination.libFolderName,
 //        config.destination.assetsFolder + config.destination.viewsFolderName,
@@ -59,7 +60,10 @@ gulp.task('watch', ['build'],  function() {
 //    gulp.watch(config.source.fontIcon, ['fonticon']);
 //    gulp.watch(config.source.fontFileList, ['fonts']);
 //    gulp.watch(config.source.imageFileList, ['images']);
-//    gulp.watch(config.source.cssWatchFileList, ['less', 'lesshint']);
+//    gulp.watch(config.source.lessWatchFileList, ['less']);
+    watch(config.source.lessWatchFileList, function() {
+        return runSequence(['less']);
+    })
 //    gulp.watch(config.source.libFileList, ['lib']);
 //    gulp.watch(config.source.sassWatchFileList, ['sass']);
 //    gulp.watch(config.source.jsFileList, ['scripts']);
