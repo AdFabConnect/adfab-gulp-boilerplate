9499e883d106aabc3d50a933f40c4bdf0d719c58
Benjamin Goetz <benjamin.goetz@adfab.fr>
2016-11-21 19:00:37 +0100

    fix: BOILERPLATE-16 - Missed a file correcting paths to the config file

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -1,4 +1,4 @@
-const config       = require('../gulp-config');
+const config       = require('../../../gulp-config');
 const gulp         = require('gulp');
 const plumber      = require('gulp-plumber');
 const sass         = require('gulp-sass');
