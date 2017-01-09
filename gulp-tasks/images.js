0635994f3b243345c6ee875ae7fc3e883a3d701d
Bruno-Pierre Campeau <zerod.zunaro@gmail.com>
2017-01-09 15:05:06 -0500

    [feature] BOILERPLATE-30 Added imagemin to the server

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,9 +1,11 @@
 var config        = require('../../../gulp-config');
 var gulp          = require('gulp');
+var imagemin      = require('gulp-imagemin');
 
 // TODO : optimize images with gulp-imagemin and gulp-svgmin
 
 module.exports = function() {
   return gulp.src(config.source.imageFileList)
+    .pipe(imagemin())
     .pipe(gulp.dest(config.destination.assetsFolder + config.destination.imagesFolderName));
 };
