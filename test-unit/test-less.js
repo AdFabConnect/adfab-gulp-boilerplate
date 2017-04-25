2e7670e896611e78d43bff94057c51b14333e3c0
jonathan-vallet <jonathan.vallet@adfab.fr>
2017-04-25 12:36:45 +0200

    [enhancement] BOILERPLATE-54: Improved unit test config management

--- test-unit/test-less.js
+++ test-unit/test-less.js
@@ -1,52 +1,62 @@
 'use strict'
 
-var config      = require('../gulp-config')
-var assert = require('assert')
+var config = {
+    tasks: {
+        less: {
+            browsers: ['last 2 versions']
+        }
+    }
+};
 
-var fromString = require('./helpers/pipe')
-var chai = require('chai')
-chai.use(require('chai-fs'))
+const assert = require('assert');
+const fromString = require('./helpers/pipe');
+const chai = require('chai');
+const expect = chai.expect;
+const gulp = require('gulp');
+const util = require('gulp-util');
 
-var expect = chai.expect
+util.env.boilerplate = {
+    config
+};
 
-var gulp = require('gulp')
-
-const lessPipe = require('../pipe/less')
-const util         = require('gulp-util')
+chai.use(require('chai-fs'));
+const lessPipe = require('../pipe/less');
 
 describe('gulp less', function() {
-  describe('compiled less', function() {
-    it('generates correct css output', function() {
-      var input = '@color: red; body { background: @color; }'
-      var result = 'test'
-      return fromString(input, 'less/style.less', lessPipe).then(output => {
-        var result = output.contents.toString()
-        var fs = require('fs')
-        fs.writeFile('test.css', result)
-        expect(result).to.equal('body {\n  background: red;\n}\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLGdCQUFnQjtDQUNqQiIsImZpbGUiOiJsZXNzL3N0eWxlLmNzcyIsInNvdXJjZXNDb250ZW50IjpbImJvZHkge1xuICBiYWNrZ3JvdW5kOiByZWQ7XG59XG4iXX0= */\n')
-      })
-    })
+    describe('compiled less', function() {
+        it('generates correct css output', function() {
+            var input = '@color: red; body { background: @color; }'
+            var result = 'test'
+            return fromString(input, 'less/style.less', lessPipe).then(output => {
+                var result = output.contents.toString()
+                var fs = require('fs')
+                fs.writeFile('test.css', result)
+                expect(result).to.equal('body {\n  background: red;\n}\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLGdCQUFnQjtDQUNqQiIsImZpbGUiOiJsZXNzL3N0eWxlLmNzcyIsInNvdXJjZXNDb250ZW50IjpbImJvZHkge1xuICBiYWNrZ3JvdW5kOiByZWQ7XG59XG4iXX0= */\n')
+                fs.unlink('test.css');
+            });
+        });
 
-    it('adds prefixes', function() {
-      var input = 'body { appearance: button; }'
-      return fromString(input, 'sass/style.scss', lessPipe).then(output => {
-        var result = output.contents.toString()
-        var fs = require('fs')
-        fs.writeFile('test.css', result)
-        expect(result).to.equal('body {\n  -webkit-appearance: button;\n     -moz-appearance: button;\n          appearance: button;\n}\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLDJCQUFtQjtLQUFuQix3QkFBbUI7VUFBbkIsbUJBQW1CO0NBQ3BCIiwiZmlsZSI6InNhc3Mvc3R5bGUuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiYm9keSB7XG4gIGFwcGVhcmFuY2U6IGJ1dHRvbjtcbn1cbiJdfQ== */\n')
-      })
-    })
+        it('adds prefixes', function() {
+            var input = 'body { appearance: button; }'
+            return fromString(input, 'sass/style.scss', lessPipe).then(output => {
+                var result = output.contents.toString()
+                var fs = require('fs')
+                fs.writeFile('test.css', result)
+                expect(result).to.equal('body {\n  -webkit-appearance: button;\n     -moz-appearance: button;\n          appearance: button;\n}\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLDJCQUFtQjtLQUFuQix3QkFBbUI7VUFBbkIsbUJBQW1CO0NBQ3BCIiwiZmlsZSI6InNhc3Mvc3R5bGUuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiYm9keSB7XG4gIGFwcGVhcmFuY2U6IGJ1dHRvbjtcbn1cbiJdfQ== */\n')
+                fs.unlink('test.css');
+            });
+        });
 
-    it('minifies and remove sourcemaps in production mod', function() {
-      var input = 'body { appearance: button; }'
-      util.env.production = true
-      return fromString(input, 'sass/style.scss', lessPipe).then(output => {
-        var result = output.contents.toString()
-        var fs = require('fs')
-        fs.writeFile('test.css', result)
-        expect(result).to.equal('body{-webkit-appearance:button;-moz-appearance:button;appearance:button}')
-      })
-    })
-  })
-})
-
+        it('minifies and remove sourcemaps in production mod', function() {
+            var input = 'body { appearance: button; }'
+            util.env.production = true
+            return fromString(input, 'sass/style.scss', lessPipe).then(output => {
+                var result = output.contents.toString()
+                var fs = require('fs')
+                fs.writeFile('test.css', result)
+                expect(result).to.equal('body{-webkit-appearance:button;-moz-appearance:button;appearance:button}')
+                fs.unlink('test.css');
+            });
+        });
+    });
+});
--- test-unit/test-sass.js
+++ test-unit/test-sass.js
@@ -1,50 +1,62 @@
 'use strict'
 
-var config      = require('../gulp-config')
-var assert = require('assert')
+var config = {
+    tasks: {
+        sass: {
+            browsers: ['last 2 versions']
+        }
+    }
+};
 
-var fromString = require('./helpers/pipe')
-var chai = require('chai')
-chai.use(require('chai-fs'))
+const assert = require('assert');
+const fromString = require('./helpers/pipe');
+const chai = require('chai');
+const expect = chai.expect;
+const gulp = require('gulp');
+const util = require('gulp-util');
 
-var expect = chai.expect
+util.env.boilerplate = {
+    config
+};
 
-var gulp = require('gulp')
+chai.use(require('chai-fs'));
 
-const sassPipe = require('../pipe/sass')
-const util         = require('gulp-util')
+const sassPipe = require('../pipe/sass');
 
 describe('gulp sass', function() {
-  describe('compiled sass', function() {
-    it('generates correct css output, generates sourcemaps', function() {
-      var input = '$color: red; body { background: $color; }'
-      return fromString(input, 'sass/style.scss', sassPipe).then(output => {
-        var result = output.contents.toString()
-        var fs = require('fs')
-        fs.writeFile('test.css', result)
-        expect(result).to.equal('body {\n  background: red; }\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLGdCQUFnQixFQUFFIiwiZmlsZSI6InNhc3Mvc3R5bGUuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiYm9keSB7XG4gIGJhY2tncm91bmQ6IHJlZDsgfVxuIl19 */\n')
-      })
-    })
-      
-    it('adds prefixes', function() {
-      var input = 'body { appearance: button; }'
-      return fromString(input, 'sass/style.scss', sassPipe).then(output => {
-        var result = output.contents.toString()
-        var fs = require('fs')
-        fs.writeFile('test.css', result)
-        expect(result).to.equal('body {\n  -webkit-appearance: button;\n     -moz-appearance: button;\n          appearance: button; }\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLDJCQUFtQjtLQUFuQix3QkFBbUI7VUFBbkIsbUJBQW1CLEVBQUUiLCJmaWxlIjoic2Fzcy9zdHlsZS5jc3MiLCJzb3VyY2VzQ29udGVudCI6WyJib2R5IHtcbiAgYXBwZWFyYW5jZTogYnV0dG9uOyB9XG4iXX0= */\n')
-      })
-    })
+    describe('compiled sass', function() {
+        it('generates correct css output, generates sourcemaps', function() {
+            var input = '$color: red; body { background: $color; }'
+            return fromString(input, 'sass/style.scss', sassPipe).then(output => {
+                var result = output.contents.toString()
+                var fs = require('fs')
+                fs.writeFile('test.css', result)
+                expect(result).to.equal('body {\n  background: red; }\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLGdCQUFnQixFQUFFIiwiZmlsZSI6InNhc3Mvc3R5bGUuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiYm9keSB7XG4gIGJhY2tncm91bmQ6IHJlZDsgfVxuIl19 */\n')
+                fs.unlink('test.css');
+            });
+        });
+            
+        it('adds prefixes', function() {
+            var input = 'body { appearance: button; }'
+            return fromString(input, 'sass/style.scss', sassPipe).then(output => {
+                var result = output.contents.toString()
+                var fs = require('fs')
+                fs.writeFile('test.css', result)
+                expect(result).to.equal('body {\n  -webkit-appearance: button;\n     -moz-appearance: button;\n          appearance: button; }\n\n/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLDJCQUFtQjtLQUFuQix3QkFBbUI7VUFBbkIsbUJBQW1CLEVBQUUiLCJmaWxlIjoic2Fzcy9zdHlsZS5jc3MiLCJzb3VyY2VzQ29udGVudCI6WyJib2R5IHtcbiAgYXBwZWFyYW5jZTogYnV0dG9uOyB9XG4iXX0= */\n')
+                fs.unlink('test.css');
+            });
+        });
 
-    it('minifies and remove sourcemaps in production mod', function() {
-      var input = 'body { appearance: button; }'
-      util.env.production = true
-      return fromString(input, 'sass/style.scss', sassPipe).then(output => {
-        var result = output.contents.toString()
-        var fs = require('fs')
-        fs.writeFile('test.css', result)
-        expect(result).to.equal('body{-webkit-appearance:button;-moz-appearance:button;appearance:button}')
-      })
-    })
-  })
-})
+        it('minifies and remove sourcemaps in production mod', function() {
+            var input = 'body { appearance: button; }'
+            util.env.production = true
+            return fromString(input, 'sass/style.scss', sassPipe).then(output => {
+                var result = output.contents.toString()
+                var fs = require('fs')
+                fs.writeFile('test.css', result)
+                expect(result).to.equal('body{-webkit-appearance:button;-moz-appearance:button;appearance:button}')
+                fs.unlink('test.css');
+            });
+        });
+    });
+});
