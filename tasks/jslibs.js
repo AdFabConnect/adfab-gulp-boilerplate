4c7b582619b1d3e1014dfecc29ba0efa7a5e4824
Mikael Lharidon <mikael.lharidon@adfab.fr>
2017-05-17 14:27:15 +0200

    build library packages with browserify externals

--- tasks/jslibs.js
+++ tasks/jslibs.js
@@ -1,14 +1,45 @@
 module.exports = function() {
     const gulp          = require('gulp');
     const notify = require('gulp-notify');
-    const concat = require('gulp-concat');
+    const browserify = require('browserify');
+    const uglify = require('gulp-uglify');
+    const source = require('vinyl-source-stream');
+    const buffer = require('vinyl-buffer');
     const util = require('gulp-util');
 
     const config = util.env.boilerplate.config;
     const jslibsConfig = config.tasks.jslibs;
 
-    return gulp.src(jslibsConfig.source, {cwd: config.sourceRoot})
-        .pipe(concat(jslibsConfig.destinationFile))
+
+    // Error notifications
+    var handleError = function(task) {
+        return function(err) {
+            notify.onError({
+                message: task + ' failed, check the logs..'
+            })(err);
+            util.log(util.colors.bgRed(task + ' error:'), util.colors.red(err));
+
+            this.emit('end');
+        };
+    };
+
+    var bundler = browserify({
+        debug: !util.env.production,
+        cache: {},
+        packageCache: {},
+    });
+
+    jslibsConfig.source.forEach(function(lib) {
+        bundler.require(lib);
+    });
+
+    return bundler.bundle()
+        .on('error', handleError('JS'))
+        .pipe(source(jslibsConfig.destinationFile))
+        .pipe(buffer())
+        .pipe(uglify().on('error', function(err) {
+            util.log(util.colors.bgRed('UglifyJS error:'), util.colors.red(err));
+        }))
         .pipe(gulp.dest(config.destinationRoot + jslibsConfig.destination))
-        .pipe(notify('Successfully compiled JS libs'));
+        .pipe(notify({message: 'Successfully compiled JS libs', onLast: true}));
 };
